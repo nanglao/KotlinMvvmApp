7c073b1a343d167ae84f3d25897c47460dd06368
mavis <nanglaokhay2015@gmail.com>
2021-01-15 17:10:08 +0900

    coroution with mvvm

--- app/src/main/java/com/dev/kotlinmvvmapp/repository/Repository.kt
+++ app/src/main/java/com/dev/kotlinmvvmapp/repository/Repository.kt
@@ -1,18 +1,35 @@
 package com.dev.kotlinmvvmapp.repository
 
 import androidx.lifecycle.LiveData
+import com.dev.kotlinmvvmapp.api.MyRetrofitBuilder
 import com.dev.kotlinmvvmapp.model.User
-import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 object Repository {
 
     var job: CompletableJob? = null
 
     fun getUser(userId: String): LiveData<User>{
+        job = Job()
         return object: LiveData<User>(){
             override fun onActive() {
                 super.onActive()
+                job?.let {theJob->
+                    CoroutineScope(IO + theJob).launch {
+                        val user = MyRetrofitBuilder.apiService.getUser(userId)
+                        withContext(Main){
+                            value = user
+                            theJob.complete()
+                        }
+                    }
+                }
             }
         }
+    }
+
+    fun cancelJobs(){
+        job?.cancel()
     }
 }
