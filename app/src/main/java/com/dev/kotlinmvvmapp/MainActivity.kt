7114f38143647b52d524d560cc79fa9dbbef0666
mavis <nanglaokhay2015@gmail.com>
2021-01-19 17:31:54 +0900

    MainViewModelを作成

--- app/src/main/java/com/dev/kotlinmvvmapp/MainActivity.kt
+++ app/src/main/java/com/dev/kotlinmvvmapp/MainActivity.kt
@@ -2,13 +2,30 @@ package com.dev.kotlinmvvmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewPropertyAnimator
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.dev.kotlinmvvmapp.model.UserData
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("DEBUG: ${UserData.userData}")
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.user.observe(this, Observer { user->
+            println("DEBUG: ${user}")
+        })
+
+        viewModel.setUserId("1")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.cancelJobs()
     }
 }
